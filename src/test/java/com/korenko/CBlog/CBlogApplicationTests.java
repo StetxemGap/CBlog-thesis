0410ff467d7fdc3c4afecb436e008fddf59449c3
StetXem <sisisisi823@gmail.com>
2025-04-10 17:35:57 +0700

    подключение spring

---
+++ src/test/java/com/korenko/CBlog/CBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korenko.CBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
