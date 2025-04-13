dec9e5afdd63967fbd6917afd3046babc4477f85
StetXem <sisisisi823@gmail.com>
2025-04-13 15:13:17 +0700

    добавлен thymeleaf, с помощью него сделаны общие navbar и sidebar

--- src/main/java/com/korenko/CBlog/CBlogApplication.java
+++ src/main/java/com/korenko/CBlog/CBlogApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class CBlogApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CBlogApplication.class, args);
 	}
 
--- src/main/java/com/korenko/CBlog/controllers/MyRestController.java
+++ src/main/java/com/korenko/CBlog/controllers/MyRestController.java
@@ -1,12 +1,40 @@
 package com.korenko.CBlog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.time.LocalTime;
+
+@Controller
 public class MyRestController {
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World 123";
+
+    @GetMapping("/auth")
+    public String auth(Model model) {
+        String helloString = "Здравствуйте!";
+
+        LocalTime now = LocalTime.now();
+        LocalTime morning = LocalTime.of(5, 0);
+        LocalTime day = LocalTime.of(12, 0);
+        LocalTime evening = LocalTime.of(18, 0);
+        LocalTime night = LocalTime.of(23, 0);
+
+        if (now.isAfter(morning) && now.isBefore(day)) {
+            helloString = "Доброго утра!";
+        } else if (now.isAfter(day) && now.isBefore(evening)) {
+            helloString = "Доброго дня!";
+        } else if (now.isAfter(evening) && now.isBefore(night)) {
+            helloString = "Доброго вечера!";
+        } else if (now.isAfter(night) && now.isBefore(morning)) {
+            helloString = "Доброй ночи!";
+        }
+
+        model.addAttribute("today", helloString);
+        return "auth";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
     }
 }
